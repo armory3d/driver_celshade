be3b446f6d97fd5688bccee12cecf2a08af85820
luboslenco <lubos.lenco@gmail.com>
2018-03-28 00:24:13 +0200

    0.3

--- blender.py
+++ blender.py
@@ -12,24 +12,131 @@ def register():
 def draw_props(layout):
     rpdat = arm.utils.get_rp()
 
-    layout.prop(rpdat, "rp_shadowmap")
-    if rpdat.rp_shadowmap != 'Off':
-        layout.prop(rpdat, "rp_shadowmap_cascades")
-    layout.prop(rpdat, "rp_background")
-    layout.prop(rpdat, "rp_hdr")
-    layout.prop(rpdat, "rp_stereo")
+    layout.label('Renderer')
+    box = layout.box().column()
+    row = box.row()
+    box.prop(rpdat, 'arm_samples_per_pixel')
+    box.prop(rpdat, 'arm_texture_filter')
+    box.prop(rpdat, 'arm_skin')
+    row = box.row()
+    row.enabled = rpdat.arm_skin.startswith('GPU')
+    row.prop(rpdat, 'arm_skin_max_bones_auto')
+    row = box.row()
+    row.enabled = not rpdat.arm_skin_max_bones_auto
+    row.prop(rpdat, 'arm_skin_max_bones')
+    row = box.row()
+    row.prop(rpdat, "rp_hdr")
+    row.prop(rpdat, "rp_stereo")
+    row = box.row()
+    row.prop(rpdat, 'arm_displacement')
+    row.prop(rpdat, 'arm_culling')
+
+    layout.label('Shadows')
+    box = layout.box().column()
+    box.prop(rpdat, 'rp_shadowmap')
+    col = box.column()
+    col.enabled = rpdat.rp_shadowmap != 'Off'
+    col.prop(rpdat, 'rp_shadowmap_cascades')
+    col2 = col.column()
+    col2.enabled = rpdat.rp_shadowmap_cascades != '1'
+    col2.prop(rpdat, 'arm_shadowmap_split')
+    col.prop(rpdat, 'arm_pcfsize')
+
+    layout.label('World')
+    box = layout.box().column()
+    row = box.row()
+    row.prop(rpdat, "rp_background", expand=True)
+    row = box.row()
+    row.prop(rpdat, 'arm_irradiance')
+    col = row.column()
+    col.enabled = rpdat.arm_irradiance
+    col.prop(rpdat, 'arm_radiance')
+    row = box.row()
+    row.enabled = rpdat.arm_irradiance
+    col = row.column()
+    col.prop(rpdat, 'arm_radiance_sky')
+    colb = row.column()
+    colb.enabled = rpdat.arm_radiance
+    colb.prop(rpdat, 'arm_radiance_size')
+    box.prop(rpdat, 'arm_clouds')
+    col = box.column()
+    col.enabled = rpdat.arm_clouds
+    row = col.row(align=True)
+    row.alignment = 'EXPAND'
+    row.prop(rpdat, 'arm_clouds_density')
+    row.prop(rpdat, 'arm_clouds_size')
+    row = col.row(align=True)
+    row.alignment = 'EXPAND'
+    row.prop(rpdat, 'arm_clouds_lower')
+    row.prop(rpdat, 'arm_clouds_upper')
+    row = col.row(align=True)
+    row.alignment = 'EXPAND'
+    row.prop(rpdat, 'arm_clouds_precipitation')
+    row.prop(rpdat, 'arm_clouds_eccentricity')
+    col.prop(rpdat, 'arm_clouds_secondary')
+    row = col.row()
+    row.prop(rpdat, 'arm_clouds_wind')
+    
     layout.separator()
     layout.prop(rpdat, "rp_render_to_texture")
-    if rpdat.rp_render_to_texture:
-        layout.prop(rpdat, "rp_supersampling")
-        layout.prop(rpdat, "rp_antialiasing")
-        layout.prop(rpdat, "rp_compositornodes")
-        layout.prop(rpdat, "rp_volumetriclight")
-        layout.prop(rpdat, "rp_bloom")
-    layout.prop(rpdat, 'arm_samples_per_pixel')
-    layout.prop(rpdat, 'arm_texture_filter')
-    layout.prop(rpdat, 'arm_displacement')
-    layout.prop(rpdat, 'arm_clouds')
+    box = layout.box().column()
+    box.enabled = rpdat.rp_render_to_texture
+    row = box.row()
+    row.prop(rpdat, "rp_antialiasing", expand=True)
+    box.prop(rpdat, "rp_supersampling")
+    box.prop(rpdat, 'arm_rp_resolution')
+    box.separator()
+    box.prop(rpdat, "rp_bloom")
+    col = box.column()
+    col.enabled = rpdat.rp_bloom
+    row = col.row(align=True)
+    row.alignment = 'EXPAND'
+    row.prop(rpdat, 'arm_bloom_threshold')
+    row.prop(rpdat, 'arm_bloom_strength')
+    col.prop(rpdat, 'arm_bloom_radius')
+    box.separator()
+    box.prop(rpdat, "rp_volumetriclight")
+    row = box.row(align=True)
+    row.alignment = 'EXPAND'
+    row.enabled = rpdat.rp_volumetriclight
+    row.prop(rpdat, 'arm_volumetric_light_air_color', text="")
+    row.prop(rpdat, 'arm_volumetric_light_air_turbidity', text="Turbidity")
+    row.prop(rpdat, 'arm_volumetric_light_steps', text="Steps")
+
+    layout.prop(rpdat, "rp_compositornodes")
+    box = layout.box().column()
+    box.enabled = rpdat.rp_compositornodes
+    box.prop(rpdat, 'arm_tonemap')
+    box.prop(rpdat, 'arm_letterbox')
+    col = box.column()
+    col.enabled = rpdat.arm_letterbox
+    col.prop(rpdat, 'arm_letterbox_size')
+    box.prop(rpdat, 'arm_sharpen')
+    col = box.column()
+    col.enabled = rpdat.arm_sharpen
+    col.prop(rpdat, 'arm_sharpen_strength')
+    box.prop(rpdat, 'arm_fisheye')
+    box.prop(rpdat, 'arm_vignette')
+    box.prop(rpdat, 'arm_lensflare')
+    box.prop(rpdat, 'arm_grain')
+    col = box.column()
+    col.enabled = rpdat.arm_grain
+    col.prop(rpdat, 'arm_grain_strength')
+    box.prop(rpdat, 'arm_fog')
+    col = box.column()
+    col.enabled = rpdat.arm_fog
+    row = col.row(align=True)
+    row.alignment = 'EXPAND'
+    row.prop(rpdat, 'arm_fog_color', text="")
+    row.prop(rpdat, 'arm_fog_amounta', text="A")
+    row.prop(rpdat, 'arm_fog_amountb', text="B")
+    box.separator()
+    box.prop(rpdat, "rp_autoexposure")
+    col = box.column()
+    col.enabled = rpdat.rp_autoexposure
+    col.prop(rpdat, 'arm_autoexposure_strength', text='Strength')
+    box.prop(rpdat, 'arm_lens_texture')
+    box.prop(rpdat, 'arm_lut_texture')
 
 def make_rpass(rpass):
     if rpass == 'mesh':
@@ -50,7 +157,7 @@ def make_mesh_pass(rpass):
     tese = None
 
     vert.add_uniform('mat3 N', '_normalMatrix')
-    vert.write_main_header('vec4 spos = vec4(pos, 1.0);')
+    vert.write_attrib('vec4 spos = vec4(pos, 1.0);')
     frag.ins = vert.outs
 
     frag.add_include('compiled.glsl')
@@ -119,7 +226,7 @@ def make_mesh_pass(rpass):
     else:
         vert.add_out('vec3 wnormal')
         make_mesh.write_norpos(con_mesh, vert)
-        frag.prepend_header('vec3 n = normalize(wnormal);')
+        frag.write_attrib('vec3 n = normalize(wnormal);')
 
     frag.add_out('vec4 fragColor')
     frag.write('vec3 direct = basecol * step(0.5, dotNL) * visibility * lightColor;')
@@ -162,17 +269,17 @@ def make_rpath():
         if rpdat.rp_compositornodes:
             assets.add_khafile_def('rp_compositornodes')
             compo_depth = False
-            if wrd.arm_tonemap != 'Off':
-                wrd.compo_defs = '_CTone' + wrd.arm_tonemap
+            if rpdat.arm_tonemap != 'Off':
+                wrd.compo_defs = '_CTone' + rpdat.arm_tonemap
             if rpdat.rp_antialiasing == 'FXAA':
                 wrd.compo_defs += '_CFXAA'
-            if wrd.arm_letterbox:
+            if rpdat.arm_letterbox:
                 wrd.compo_defs += '_CLetterbox'
-            if wrd.arm_grain:
+            if rpdat.arm_grain:
                 wrd.compo_defs += '_CGrain'
             if bpy.data.scenes[0].cycles.film_exposure != 1.0:
                 wrd.compo_defs += '_CExposure'
-            if wrd.arm_fog:
+            if rpdat.arm_fog:
                 wrd.compo_defs += '_CFog'
                 compo_depth = True
             if len(bpy.data.cameras) > 0 and bpy.data.cameras[0].dof_distance > 0.0:
@@ -181,18 +288,20 @@ def make_rpath():
             if compo_depth:
                 wrd.compo_defs += '_CDepth'
                 assets.add_khafile_def('rp_compositordepth')
-            if wrd.arm_lens_texture != '':
+            if rpdat.arm_lens_texture != '':
                 wrd.compo_defs += '_CLensTex'
                 assets.add_embedded_data('lenstexture.jpg')
-            if wrd.arm_fisheye:
+            if rpdat.arm_fisheye:
                 wrd.compo_defs += '_CFishEye'
-            if wrd.arm_vignette:
+            if rpdat.arm_vignette:
                 wrd.compo_defs += '_CVignette'
-            if wrd.arm_lensflare:
+            if rpdat.arm_lensflare:
                 wrd.compo_defs += '_CGlare'
-            if wrd.arm_lut_texture != '':
+            if rpdat.arm_lut_texture != '':
                 wrd.compo_defs += '_CLUT'
                 assets.add_embedded_data('luttexture.jpg')
+            if '_CDOF' in wrd.compo_defs or '_CFXAA' in wrd.compo_defs or '_CSharpen' in wrd.compo_defs:
+                wrd.compo_defs += '_CTexStep'
             assets.add_shader_pass('compositor_pass')
         else:
             assets.add_shader_pass('copy_pass')
